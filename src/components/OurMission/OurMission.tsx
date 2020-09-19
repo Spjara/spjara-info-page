a50c08045269dc90607c98005a4de6f1798e8e2d
palaogn <palaogn@gmail.com>
2020-09-19 18:03:08 +0000

    added some text to our mission page

--- src/components/OurMission/OurMission.tsx
+++ src/components/OurMission/OurMission.tsx
@@ -18,11 +18,24 @@ const Title = styled.div`
   letter-spacing: 5px;
 `;
 
+const SubTitle = styled.div`
+  font-size: 25px;
+  color: #6a90a3;
+  letter-spacing: 3px;
+  margin-top: 10%;
+  max-width: 600px;
+  margin: 10% auto 0 auto;
+`;
+
 function OurMission() {
   return (
     <Wrapper>
       <Content>
         <Title>Markmið okkar</Title>
+        <SubTitle>
+          SPJARA er stafrænn vettvangur og þar sem notendur geta bæði leigt út
+          og fengið leigðar flíkur, eins og Airbnb fyir föt.
+        </SubTitle>
       </Content>
     </Wrapper>
   );
